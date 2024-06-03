9044783a6373a0da6e7f7b29d9010dfdb8b1019d
Jordan <jordan@smartdust.me>
2024-06-03 12:00:36 +0200

    fix

--- namenode/run.sh
+++ namenode/run.sh
@@ -10,6 +10,7 @@ python3 -m venv /home/bigd/sparkvenv
 source /home/bigd/sparkvenv/bin/activate
 pip install pyspark pyspark[sql] matplotlib pillow venv-pack
 venv-pack -o /home/bigd/sparkvenv.tar.gz
+chown bigd /home/bigd/sparkvenv.tar.gz
 
 /home/bigd/nifi-1.26.0/bin/nifi.sh start
 /home/bigd/nifi-1.26.0/bin/nifi.sh set-single-user-credentials bigd bigdbigdbigd
