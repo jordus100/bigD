a501c883ea1f23d7123b44875f95ca37a805841f
Jordan <jordan@smartdust.me>
2024-06-03 11:08:46 +0200

    Add packing sparkvenv

--- namenode/run.sh
+++ namenode/run.sh
@@ -8,7 +8,8 @@ env | egrep -v "^(HOME=|USER=|MAIL=|LC_ALL=|LS_COLORS=|LANG=|HOSTNAME=|PWD=|TERM
 # Create Python venv for Apache Spark
 python3 -m venv /home/bigd/sparkvenv
 source /home/bigd/sparkvenv/bin/activate
-pip install pyspark pyspark[sql] matplotlib pillow
+pip install pyspark pyspark[sql] matplotlib pillow venv-pack
+venv-pack -o /opt/spark/sparkvenv.tar.gz
 
 /home/bigd/nifi-1.26.0/bin/nifi.sh start
 /home/bigd/nifi-1.26.0/bin/nifi.sh set-single-user-credentials bigd bigdbigdbigd
